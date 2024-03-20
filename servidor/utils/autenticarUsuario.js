e9c79ba2f77cf1307c4479c8b6977134d6d451b7
mateus <mateuscayres3@gmail.com>
2024-03-20 14:44:42 -0400

    fix:fix

--- servidor/utils/autenticarUsuario.js
+++ servidor/utils/autenticarUsuario.js
@@ -1,8 +1,8 @@
 import {scryptSync, timingSafeEqual} from 'crypto'
 function autenticarUsuario(senhaDigitada, usuario){
   const hashTeste = scryptSync(senhaDigitada,usuario.salSenha, 64);
-  const hashreal = Buffer.from(usuario.hashSenha, "hex");
-  const autenticado = timingSafeEqual(hashTeste,hashreal);
+  const hashReal = Buffer.from(usuario.hashSenha, "hex");
+  const autenticado = timingSafeEqual(hashTeste,hashReal);
   return autenticado;
 }
 
