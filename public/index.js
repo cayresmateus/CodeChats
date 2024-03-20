c473c7e0bfa6ebdeacb083ebf43ba850ec9e43d9
mateus <mateuscayres3@gmail.com>
2024-03-20 16:08:46 -0400

    feat: logout

--- public/index.js
+++ public/index.js
@@ -1,8 +1,18 @@
 import { emitirAdicionarDocumento } from "./socket-front-index.js";
+import { obterCookie,removerCokie } from "./utils/cookies.js";
+
+const tokenJwt = obterCookie("token");
+console.log(tokenJwt);
 
 const listaDocumentos = document.getElementById("lista-documentos");
 const form = document.getElementById("form-adiciona-documento");
 const inputDocumento = document.getElementById("input-documento");
+const botaoLogout = document.getElementById("botao-logout");
+
+botaoLogout.addEventListener("click", () => {
+removerCokie("token"); 
+window.location.href = "/login/index.html"   
+})
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
