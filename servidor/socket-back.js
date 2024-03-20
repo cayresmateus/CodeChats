f10fa1d8aa74efb5ce4398e74109b3890d2968c0
mateus <mateuscayres3@gmail.com>
2024-03-20 14:12:26 -0400

    feat: registrando evento de login back

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,12 +1,13 @@
 import registrarEventosCadastro from "./registraEventos/cadastro.js";
 import registrarEventosDocumento from "./registraEventos/documento.js";
 import registrarEventosInicio from "./registraEventos/inicio.js";
+import registraEventosLogin from "./registraEventos/login.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
+  registrarEventosCadastro(socket,io);
+  registraEventosLogin(socket,io);
   registrarEventosInicio(socket,io);
   registrarEventosDocumento(socket,io);
-  registrarEventosCadastro(socket,io)
-
  
 });
