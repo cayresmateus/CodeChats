ada9273bf9c1fe079c94dac5d7dc889e49ae2f60
mateus <mateuscayres3@gmail.com>
2024-03-20 16:09:07 -0400

    feat: guardando cookies no navegador

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -1,9 +1,12 @@
+import {definirCookie} from "../utils/cookies.js";
+
 const socket = io();
 
 function emitirAutenticarUsuario(dados){
   socket.emit("autenticar_usuario", dados);
 }
-socket.on("autenticacao_sucesso", () => {
+socket.on("autenticacao_sucesso", (token) => {
+  definirCookie("token", token)
   alert("Usuário autenticado com sucesso");
   window.location.href = "/"
 })
