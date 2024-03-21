d52c9bd08b10ee841e773746984585cdc0ce3af3
mateus <mateuscayres3@gmail.com>
2024-03-21 17:17:25 -0400

    feat: definindo nsp usuarios

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -4,11 +4,20 @@ import registrarEventosDocumento from "./registraEventos/documento.js";
 import registrarEventosInicio from "./registraEventos/inicio.js";
 import registraEventosLogin from "./registraEventos/login.js";
 import io from "./servidor.js";
+import autorizarUsuario from "./middlewares/autorizarUsuario.js";
 
-io.on("connection", (socket) => {
+const nspUsuarios = io.of("/usuarios");
+
+nspUsuarios.use(autorizarUsuario);
+
+nspUsuarios.on("connection", (socket) => {
+  registrarEventosInicio(socket,nspUsuarios);
+  registrarEventosDocumento(socket,nspUsuarios);
+})
+
+io.of("/").on("connection", (socket) => {
   registrarEventosCadastro(socket,io);
   registraEventosLogin(socket,io);
-  registrarEventosInicio(socket,io);
-  registrarEventosDocumento(socket,io);
+  
  
 });
