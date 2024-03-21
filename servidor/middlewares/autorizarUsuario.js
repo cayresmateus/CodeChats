595f2475f2017d03a409f6a2c98e06de231495b0
mateus <mateuscayres3@gmail.com>
2024-03-21 17:16:38 -0400

    util function autorizar users

---
+++ servidor/middlewares/autorizarUsuario.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+
+function autorizarUsuario(socket,next){
+  const token = socket.handshake.auth.token;
+  try {
+    const payloadToken = jwt.verify(token, process.env.SEGREDO_JWT);
+    
+    next();
+
+  }catch (erro) {
+    next(erro)       
+  }
+  
+}
+
+export default autorizarUsuario;
