94d964c845f62d79d2326bf9b5a67ed9a6bd44df
mateus <mateuscayres3@gmail.com>
2024-03-23 13:15:17 -0400

    trantando autorizações

--- servidor/middlewares/autorizarUsuario.js
+++ servidor/middlewares/autorizarUsuario.js
@@ -4,7 +4,7 @@ function autorizarUsuario(socket,next){
   const token = socket.handshake.auth.token;
   try {
     const payloadToken = jwt.verify(token, process.env.SEGREDO_JWT);
-    
+    socket.emit("autorizacao_sucesso", payloadToken);
     next();
 
   }catch (erro) {
